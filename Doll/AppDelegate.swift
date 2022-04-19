a31f7a368988528e3a87b06775276d7d6f7d6d6b
xiaogd <xiaogdgenuine@gmail.com>
2022-04-19 17:53:41 +0800

    Fix #4 again

--- Doll/AppDelegate.swift
+++ Doll/AppDelegate.swift
@@ -2,8 +2,6 @@ import SwiftUI
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    var appActivated = false
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         MonitorEngine.setup()
     }
@@ -11,12 +9,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillBecomeActive(_ notification: Notification) {
         // https://github.com/xiaogdgenuine/Doll/issues/4
         // Create new instance if the app relaunch again from Spotlight or manually open by user
-        if appActivated {
-            print("Activated!")
-            MonitorEngine.createNewInstanceIfNecessary()
-        }
-
-        appActivated = true
+        MonitorEngine.createNewInstanceIfNecessary()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
